d054facaffa0b1d5068ca6ac29ce71da07752f28
Sachin <sachinacharya44@gmail.com>
2024-04-28 16:23:21 +0545

    fix:fix bug in hashing

--- utils.py
+++ utils.py
@@ -2,4 +2,6 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_hash(password:str):
-    pwd_context.hash(password)
+    return pwd_context.hash(password)
+def verify(plain_password,hashed_passwword):
+    return pwd_context.verify(plain_password,hashed_passwword)
